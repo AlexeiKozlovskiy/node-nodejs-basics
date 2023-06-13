cd41a10fe6b2aa4fc36f1136b21621e2e3974a44
AlexeiKozlovskiy <gekleberefin@gmail.com>
2023-06-14 00:23:21 +0300

    feat: add solution fs:create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import { writeFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  const content = 'I am fresh and young';
+  const errorMessage = 'FS operation failed';
+  const folderName = 'files';
+  const fileName = 'fresh.txt';
+  const folderPath = path.join(__dirname, folderName, fileName);
+
+  try {
+    await writeFile(folderPath, content, { flag: 'wx' });
+  } catch (err) {
+    throw new Error(errorMessage);
+  }
 };
 
 await create();
