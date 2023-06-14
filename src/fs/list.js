2b72bd2c8bfb95afe29cd7278a4248b622a5c1ed
AlexeiKozlovskiy <gekleberefin@gmail.com>
2023-06-14 20:12:40 +0300

    feat: add solution fs:list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+import { __dirname, errorMessage, folderSourse } from './utils.js';
+import path from 'path';
+import { readdir } from 'node:fs/promises';
+
 const list = async () => {
-    // Write your code here 
+  const folderFiles = path.join(__dirname, folderSourse);
+
+  try {
+    console.log(await readdir(folderFiles));
+  } catch (err) {
+    throw new Error(errorMessage);
+  }
 };
 
 await list();
