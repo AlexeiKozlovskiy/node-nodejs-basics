1f38062c9f91d1ff4edcd6e53d9ecfd5f22a8c75
AlexeiKozlovskiy <gekleberefin@gmail.com>
2023-06-15 15:19:02 +0300

    feat: add solution cli:env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envRss = Object.entries(process.env)
+    .reduce((acc, cur) => {
+      if (cur[0].startsWith('RSS_')) {
+        acc.push(`${cur[0]}=${cur[1]}`);
+      }
+      return acc;
+    }, [])
+    .join('; ');
+  console.log(envRss);
 };
 
 parseEnv();
