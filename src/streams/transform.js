b02656851d6d00d2c6216ac359f1a6398babc90a
AlexeiKozlovskiy <gekleberefin@gmail.com>
2023-06-16 01:14:16 +0300

    feat: add solution streams:transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import { Transform } from 'node:stream';
+import { pipeline } from 'node:stream/promises';
+import { stdout } from 'process';
+
 const transform = async () => {
-    // Write your code here 
+  const transformedContent = new Transform({
+    transform(chunk, encoding, callback) {
+      const reverseContent = chunk.toString().split('').reverse().join('');
+      callback(null, `${reverseContent}\n`);
+    },
+  });
+  stdout.write('Input your data\nCtrl+C for exit\n');
+  await pipeline(process.stdin, transformedContent, process.stdout);
 };
 
 await transform();
